ecb177fcbe66f4b1f007c53f2271101c20861fb9
EoCiMrEo <eocimreo@gmail.com>
2025-06-27 19:08:17 -0400

    Fix Horizontally Move

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased overflow-x-hidden`}
       >
         <ThemeProvider
           attribute="class"
